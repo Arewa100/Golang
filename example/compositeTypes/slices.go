8be687a24b945adb938dd1baa98c95d7a4f02570
Arewa100 <olasorexway@gmail.com>
2025-07-28 04:32:34 +0100

    new update on go lang

--- example/compositeTypes/slices.go
+++ example/compositeTypes/slices.go
@@ -4,4 +4,11 @@ import("fmt")
 
 func main() {
 	fmt.Println("welcome to slices")
+
+	mySlice := make([]byte, 4, 7)
+	runes := append(mySlice, 77)
+	rune := append(mySlice, 55)
+	fmt.Println(runes)
+	fmt.Println(rune)
+	fmt.Println(mySlice)
 }
