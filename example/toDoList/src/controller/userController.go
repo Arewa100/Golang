9a4daba4382817be1ba9d850f0cdfac162545341
Arewa100 <olasorexway@gmail.com>
2025-08-30 03:50:39 +0100

    new update

--- example/toDoList/src/controller/userController.go
+++ example/toDoList/src/controller/userController.go
@@ -2,38 +2,73 @@ package main
 
 import (
 	"encoding/json"
+	//"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
-type Person struct {
-	Name    string
-	Age     int
-	Country string
+type Message struct {
+	Content string `json:"content"`
+	Id      int    `json:"id"`
 }
-
-var newPerson Person = Person{"Miracle", 25, "Nigeria"}
-var newJsonData, err = json.Marshal(newPerson)
 
 func main() {
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(newJsonData))
-	fmt.Println("quick test of my application")
-	PORT := ":8080"
+
+	//newMessage := Message{Content: "Hello World!", Id: 23}
+	//
+	//byteArray, err := json.Marshal(newMessage)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.HandlerFunc(PrintName))
+	PORT := ":8080"
 
-	serverError := http.ListenAndServe(PORT, mux)
+	mux.Handle("/", http.HandlerFunc(getMessage))
+
+	server := &http.Server{
+		Addr:         PORT,
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	serverError := server.ListenAndServe()
 	if serverError != nil {
-		log.Fatal(serverError)
+		log.Fatal(server)
 	}
 
 }
 
-func PrintName(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(response, "<h1>Hello World<h1/>", newJsonData)
+func getMessage(response http.ResponseWriter, request *http.Request) {
+	log.Println("the request is", request.URL.Path, "from", request.Host)
+	response.WriteHeader(http.StatusOK)
+	body := Message{Content: "Hello World", Id: 20}
+	jsonString, err := json.Marshal(body)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+	}
+	fmt.Fprintf(response, "%s", jsonString)
+
 }
+
+//func defaultHandler(response http.ResponseWriter, request *http.Request) {
+//	log.Println("default", request.URL.Path, "from", request.Host)
+//	response.WriteHeader(http.StatusNotFound)
+//	body := "thank you: try again later"
+//	fmt.Fprintf(response, "%s", body)
+//
+//}
+//
+//func deleteHandler(response http.ResponseWriter, request *http.Request) {
+//	parameter := strings.Split(request.URL.Path, "/")
+//	fmt.Println(parameter)
+//	response.WriteHeader(http.StatusOK)
+//	if len(parameter) < 4 {
+//		response.WriteHeader(http.StatusBadRequest)
+//
+//	}
+//}
